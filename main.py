304ddd3926f10e373161a637c25580376c23c09c
ocean12348 <68468894+ocean12348@users.noreply.github.com>
2026-02-17 08:40:41 +0700

    Fixed things
    Fixed scanline progressbars and fixed the  image output

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import io
+import sys
 
 image_width = 256
 image_height = 256
@@ -8,16 +9,18 @@ image_height = 256
 print("P3")
 print(f"{image_width} {image_height}")
 print("255")
-for (j = 0; j < image_height; j++){
-    for (i = 0; i < image_width; i++){
+for j in range(image_height):
+
+    sys.stderr.write(f"\rScanlines remaining: {(image_height - j)}                  ")
+    sys.stderr.flush()
+
+    for i in range(image_width):
         r = i/(image_height-1)
         g = j/(image_width-1)
         b = 0.0
 
-        ir = (255.999 * r)
-        ig = (255.999 * g)
-        ib = (255.999 * b)
+        ir = int(255.999 * r)
+        ig = int(255.999 * g)
+        ib = int(255.999 * b)
 
-        print({ir}, " ", {ig}, " ", {ib})
-    }
-}
+        print(ir, ig, ib)
